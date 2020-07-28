71d94a59b8a41ae1ae95b77f9bcbab3a32500737
neonzoff <neonzoff@gmail.com>
2020-07-28 21:27:20 +0300

    Added class Triangle 1/4 solved

--- src/com/sbrf/daytwo/Task4.java
+++ src/com/sbrf/daytwo/Task4.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Task4 test = new Task4();
-        test.testPoint();
+        test.testTriangle();
     }
 
     public void testCircle() {
@@ -41,6 +41,17 @@ public class Task4 {
         point2.setX(-1);
         point2.setY(5);
         System.out.println("Равны ли точки: " + point2.equals(point1));
+    }
+
+    public void testTriangle() {
+        Triangle triangle = new Triangle();
+        System.out.println("Периметр треугольника: " + triangle.perimeter());
+        triangle.print();
+        triangle.area();
+        triangle.multiply(0.5);
+        triangle.area();
+        triangle.move();
+        triangle.print();
     }
 }
 
@@ -126,7 +137,7 @@ class Circle extends Figure {
     }
 }
 
-class Point {
+class Point {                //точка - абстрактное понятие, сомневаюсь, что от Figure нужно наследоваться, нет площади и невозможно масштабировать
     private double x;
     private double y;
 
@@ -193,5 +204,67 @@ class Point {
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + x + " , " + y + " ]";
+    }
+}
+
+class Triangle extends Figure {
+    private Point p1;
+    private Point p2;
+    private Point p3;
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+
+    public Triangle() {
+        p1 = new Point();
+        p2 = new Point();
+        p3 = new Point();
+    }
+
+
+    @Override
+    void print() {
+        System.out.println("Треугольник с вершинами: " + p1 + " " + p2 + " " + p3);
+    }
+
+    @Override
+    void move() {
+        p1 = new Point();
+        p2 = new Point();
+        p3 = new Point();
+    }
+
+    @Override
+    void multiply(double coefficient) {
+        p1.setX(p1.getX() * coefficient);
+        p1.setY(p1.getY() * coefficient);
+        p2.setX(p2.getX() * coefficient);
+        p2.setY(p2.getY() * coefficient);
+        p3.setX(p3.getX() * coefficient);
+        p3.setY(p3.getY() * coefficient);
+    }
+
+    double perimeter() {
+        double ab = Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
+        double bc = Math.sqrt(Math.pow((p3.getX() - p2.getX()), 2) + Math.pow((p3.getY() - p2.getY()), 2));
+        double ca = Math.sqrt(Math.pow((p3.getX() - p1.getX()), 2) + Math.pow((p3.getY() - p1.getY()), 2));
+        return ab + bc + ca;
+    }
+
+    @Override
+    void area() {
+        double halfOfPerimeter = perimeter() / 2;
+        double ab = Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
+        double bc = Math.sqrt(Math.pow((p3.getX() - p2.getX()), 2) + Math.pow((p3.getY() - p2.getY()), 2));
+        double ca = Math.sqrt(Math.pow((p3.getX() - p1.getX()), 2) + Math.pow((p3.getY() - p1.getY()), 2));
+        System.out.println("Площадь: " + Math.sqrt(halfOfPerimeter * (halfOfPerimeter - ab) * (halfOfPerimeter - bc) * (halfOfPerimeter - ca)));
     }
 }
